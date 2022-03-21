7d3d8623e7ee0ac94bec58822489a1c664fe0300
Danny Bravo <danny.bravo18@gmail.com>
2022-03-21 01:03:00 -0400

    add menu button functionality
    Added click event for menu button to drop down nav menu.
    Adjusted scroll margins for section links

--- js/app.js
+++ js/app.js
@@ -29,3 +29,25 @@ landingPage.addEventListener('click', (e) => {
         }
     }
 })
+
+const navItems = document.querySelector(".nav-items")
+const navbar = document.querySelector(".navbar")
+const navHeader = document.querySelector(".nav-header")
+
+navbar.addEventListener("click", (e) => {
+    if (e.target.tagName === "BUTTON" || e.target.closest("#menu-btn")) {
+        if (navItems.classList.contains("inactive")) {
+            navItems.style.maxHeight = "1000px";
+            navItems.classList.remove("inactive");
+            navItems.classList.add("active");
+        } else if (navItems.classList.contains("active")) {
+            navItems.style.maxHeight = "0px";
+            navItems.classList.remove("active");
+            navItems.classList.add("inactive")
+        }
+    } else if (e.target.tagName === "A" && navItems.classList.contains("active")) {
+            navItems.style.maxHeight = "0px";
+            navItems.classList.remove("active");
+            navItems.classList.add("inactive")
+    }
+})
