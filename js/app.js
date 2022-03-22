c92b524396475f896bcd40905781630bb6a9d65a
Danny Bravo <danny.bravo18@gmail.com>
2022-03-21 21:45:47 -0400

    edit grid for cards and add socials
    Fixed sizing for cards at different viewports.
    Added images for project cards.
    Changed text for skill cards.
    Added socials div for navbar.
    Added socials to menu transition.

--- js/app.js
+++ js/app.js
@@ -30,18 +30,21 @@ landingPage.addEventListener('click', (e) => {
     }
 })
 
-const navItems = document.querySelector(".nav-items")
-const navbar = document.querySelector(".navbar")
-const navHeader = document.querySelector(".nav-header")
+const navItems = document.querySelector(".nav-items");
+const navbar = document.querySelector(".navbar");
+const navHeader = document.querySelector(".nav-header");
+const socials = document.querySelector(".socials");
 
 navbar.addEventListener("click", (e) => {
     if (e.target.tagName === "BUTTON" || e.target.closest("#menu-btn")) {
         if (navItems.classList.contains("inactive")) {
             navItems.style.maxHeight = "1000px";
+            socials.style.maxHeight = "1000px";
             navItems.classList.remove("inactive");
             navItems.classList.add("active");
         } else if (navItems.classList.contains("active")) {
             navItems.style.maxHeight = "0px";
+            socials.style.maxHeight = "0px";
             navItems.classList.remove("active");
             navItems.classList.add("inactive")
         }
