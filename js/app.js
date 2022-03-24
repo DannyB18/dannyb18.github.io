d90b2cc9bc77662d48299d4ff97760e1b8f2403e
Danny Bravo <danny.bravo18@gmail.com>
2022-03-24 00:04:19 -0400

    add collapse after selecting social link through dropdown menu

--- js/app.js
+++ js/app.js
@@ -50,6 +50,7 @@ navbar.addEventListener("click", (e) => {
         }
     } else if (e.target.tagName === "A" && navItems.classList.contains("active")) {
             navItems.style.maxHeight = "0px";
+            socials.style.maxHeight = "0px";
             navItems.classList.remove("active");
             navItems.classList.add("inactive")
     }
